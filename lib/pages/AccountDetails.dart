9ed8ee1bb0ce0538fdb0a85dc11fc40be135f2a8
atibrewal98 <ankit@siplnet.com>
2019-04-06 20:21:55 +0800

    Complete Initial Design

--- lib/pages/AccountDetails.dart
+++ lib/pages/AccountDetails.dart
@@ -98,20 +98,54 @@ class AccountDetails extends StatelessWidget {
                 )
               ),
             ),
+            // Container(
+            //   child: Container(
+            //     margin: EdgeInsets.only(top: 10.0),
+            //     child: Text(
+            //       "Account                               Current Balance",
+            //       style: TextStyle(fontFamily: 'opensans', fontSize: 18.0, color: Color.fromRGBO(186, 228, 245, 1.0), fontWeight: FontWeight.bold),
+            //     ),
+            //   ),
+            // ),
             Container(
               child: Container(
                 margin: EdgeInsets.only(top: 10.0),
-                child: Text(
-                  "Account                               Current Balance",
-                  style: TextStyle(fontFamily: 'opensans', fontSize: 18.0, color: Color.fromRGBO(186, 228, 245, 1.0), fontWeight: FontWeight.bold),
+                padding: EdgeInsets.only(left: 15.0, right: 15.0),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      // padding: EdgeInsets.only(left: 10.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Account",
+                          style: TextStyle(fontFamily: 'opensans', fontSize: 18.0, color: Color.fromRGBO(186, 228, 245, 1.0), fontWeight: FontWeight.bold),                        ),
+                      ),
+                    ),
+                    Expanded(
+                      // padding: EdgeInsets.only(left: 10.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "Current Balance",
+                          style: TextStyle(fontFamily: 'opensans', fontSize: 18.0, color: Color.fromRGBO(186, 228, 245, 1.0), fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ]
                 ),
               ),
             ),
             Container(
               child: Container(
-                margin: EdgeInsets.only(top: 10.0),
+                margin: EdgeInsets.only(top: 12.0),
                 padding: EdgeInsets.only(top: 20.0, bottom: 20.0),
-                color: Color.fromRGBO(230, 230, 230, 1.0),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.3))
+                    ),
+                    color: Color.fromRGBO(245, 245, 245, 1.0),
+                ),
                 child: Column(
                   children: <Widget>[
                     Container(
@@ -137,46 +171,279 @@ class AccountDetails extends StatelessWidget {
                 ),
               ),
             ),
-            Expanded(
+            Container(
               child: Container(
-                margin: EdgeInsets.only(left: 30.0, right: 30.0),
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintStyle: TextStyle(),
-                    prefixIcon: Icon(
-                      Icons.lock,
-                      color: Color.fromRGBO(0, 0, 0, 0.9),
+                padding: EdgeInsets.only(top: 17.0, bottom: 17.0, left: 15.0, right:15.0),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.3))
                     ),
-                    hintText: 'Password',
+                ),
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      // padding: EdgeInsets.only(left: 10.0),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          "Statement Savings",
+                          style: TextStyle(fontFamily: 'opensans', fontSize: 16.0, fontWeight: FontWeight.bold),                        ),
+                      ),
+                    ),
+                    Expanded(
+                      // padding: EdgeInsets.only(left: 10.0),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          "USD 7,309.18",
+                          style: TextStyle(fontFamily: 'opensans', fontSize: 16.0, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ]
+                ),
+              ),
+            ),
+            Container(
+              child: Container(
+                padding: EdgeInsets.only(top: 17.0, bottom: 17.0, left: 15.0, right: 15.0),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.3))
+                    ),
+                ),
+                child: Text(
+                  "The information is for reference and should not be regarded as a balance confirmation.",
+                  style: TextStyle(fontFamily: 'opensans', fontSize: 16.0),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+            ),
+            Container(
+              child: Container(
+                padding: EdgeInsets.only(top: 17.0, bottom: 17.0, left: 15.0, right: 15.0),
+                decoration: const BoxDecoration(
+                  border: Border(
+                    bottom: BorderSide(color: Color.fromRGBO(0, 0, 0, 0.3))
+                    ),
+                ),
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: Text(
+                    "Identity: John",
+                    style: TextStyle(fontFamily: 'opensans', fontSize: 16.0),
                   ),
                 ),
               ),
             ),
             Container(
-              margin: EdgeInsets.all(10.0),
-              child: SizedBox(
-                width: double.infinity,
-
-                child: Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  child: Material(
-                    borderRadius: BorderRadius.circular(30.0),
-                    shadowColor: Color.fromRGBO(186, 228, 245, 0.5),
-                    color: Color.fromRGBO(186, 228, 245, 1.0),
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      child: Text(
-                        "Log In",
-                        style:
-                            TextStyle(fontFamily: 'opensans', fontSize: 18.0),
-                      ),
-                      onPressed: () {},
-                      minWidth: 200.0,
-                      height: 42.0,
-                    ),
+              child: Container(
+                padding: EdgeInsets.only(top: 17.0, bottom: 11.0, left: 15.0, right: 15.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "Quick Access",
+                    style: TextStyle(fontFamily: 'opensans', fontSize: 16.0, fontWeight: FontWeight.bold),
                   ),
                 ),
+              ),
+            ),
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.only(left: 12.0, right: 12.0),
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                        ]
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.only(top: 15.0, bottom: 7.0),
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                          Expanded(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  child: Icon(
+                                    Icons.account_balance_wallet,
+                                    size: 40.0,
+                                  )
+                                ),
+                                Container(
+                                  alignment: Alignment.center,
+                                  child: Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                      "My Account",
+                                      style: TextStyle(fontFamily: 'opensans', fontSize: 14.0, fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ]
+                            )
+                          ),
+                        ]
+                      ),
+                    ),
+                  ]
+                )
               ),
             ),
             Container(
